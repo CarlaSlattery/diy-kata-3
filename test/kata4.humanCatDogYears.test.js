cdb0ade460e40d5518762264a68d316a9ed94006
CarlaSlattery <cslatterydev@gmail.com>
2022-10-13 11:55:47 +0100

    completed fizzBuzz kata

--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,8 @@
 const { humanCatDogYears } = require("../src");
 
 // Look Ma, no handlebars!!!
+describe("humanCatDogYears", () => {
+  it("represents human years to cats and dogs", () => {
+    expect(Array.isArray(humanCatDogYears())).toBe(true);
+  });
+});
