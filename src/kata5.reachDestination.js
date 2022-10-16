d06f2d4c5ed1a7d436f688522c5d752b5486d6ed
CarlaSlattery <cslatterydev@gmail.com>
2022-10-16 15:18:12 +0100

    passed kata 4 & 5

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,6 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+  const estimatedTime = Math.round((distance / speed) * 2) / 2;
+  return `I should be there in ${estimatedTime} hours.`;
+};
 
 module.exports = reachDestination;
--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -3,6 +3,9 @@ const { humanCatDogYears } = require("../src");
 // Look Ma, no handlebars!!!
 describe("humanCatDogYears", () => {
   it("represents human years to cats and dogs", () => {
-    expect(Array.isArray(humanCatDogYears())).toBe(true);
+    expect(humanCatDogYears(5)).toEqual([5, 36, 39]);
+  });
+  it("represents human years to cats and dogs", () => {
+    expect(humanCatDogYears(0)).toEqual([0, 0, 0]);
   });
 });
