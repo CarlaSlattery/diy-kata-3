4a042ffadf35d819335f6e631cb3c7ae1fe26584
CarlaSlattery <cslatterydev@gmail.com>
2022-10-17 11:49:18 +0100

    kata 7 test working

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,6 @@
-const joinNames = (namesObj) => {};
+const joinNames = (namesObj) => {
+  const names = Object.values(namesObj).map((i) => i.name);
+  return `${names.slice(0, -1).join(", ")} & ${names.slice(-1)}`;
+};
 
 module.exports = joinNames;
